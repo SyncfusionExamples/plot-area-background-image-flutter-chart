2059d1e80de20cb98f52269bad0409a0b179d36c
Sriram Kiran Senthilkumar <sriramkiran.senthilkumar@syncfusion.com>
2020-04-28 21:27:50 +0530

    committed

---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:syncfusion_flutter_core/core.dart';
+
+void main() {
+  // Register your license here
+  SyncfusionLicense.registerLicense(null);
+  return runApp(BackgroundImage());
+  }
+
+class BackgroundImage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chart Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Chart With Background Image'),
+        ),
+        body: SfCartesianChart(
+            plotAreaBackgroundImage:const AssetImage('images/texture.jpg'),
+            primaryXAxis: CategoryAxis(),
+            title: ChartTitle(text: 'Half yearly sales analysis'),
+            legend: Legend(isVisible: true),
+            tooltipBehavior: TooltipBehavior(enable: true),
+            series: <LineSeries<SalesData, String>>[
+              LineSeries<SalesData, String>(
+                  dataSource: <SalesData>[
+                    SalesData('Jan', 21),
+                    SalesData('Feb', 24),
+                    SalesData('Mar', 36),
+                    SalesData('Apr', 38),
+                    SalesData('May', 54)
+                  ],
+                  name: 'Sales',
+                  xValueMapper: (SalesData sales, _) => sales.year,
+                  yValueMapper: (SalesData sales, _) => sales.sales,
+                  dataLabelSettings: DataLabelSettings(isVisible: true)
+              )
+            ]
+        )
+    );
+  }
+}
+
+class SalesData {
+  SalesData(this.year, this.sales);
+
+  final String year;
+  final double sales;
+}
